dfdfe78ca0f5423fe715ab63680efe0987c131a4
MaxxKs <maks-ksenzow@mail.ru>
2024-11-06 02:02:16 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void info() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.info();
+
+        Error errorParameters = new Error(true, 12, "Без ошибок");
+        errorParameters.info();
+
+        Error parameters = new Error(false, 2, "Ошибка");
+        parameters.info();
+    }
+}
