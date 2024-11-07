73a26203586f3f546b13c81c344822e263208a86
MaxxKs <maks-ksenzow@mail.ru>
2024-11-07 19:58:37 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,8 +4,26 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public void info() {
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println();
     }
 
     public void setId(int id) {
@@ -18,5 +36,16 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item empty = new Item();
+        empty.info();
+
+        Item justName = new Item("Alexander");
+        justName.info();
+
+        Item idAndName = new Item(1, "Petr");
+        idAndName.info();
     }
 }
