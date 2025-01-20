e76577e264f42d6c6b87f15eb333f9ffa10b67e8
MaxxKs <maks-ksenzow@mail.ru>
2025-01-20 23:33:19 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,40 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                result[size] = item;
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        size = 0;
+        Item[] result1 = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null && item.getName().equals(key)) {
+                result1[size] = item;
+                size++;
+            }
+        }
+        result1 = Arrays.copyOf(result1, size);
+        for (int i = 0; i < result1.length; i++) {
+            System.out.println(result1[i].getName());
+        }
+        return result1;
     }
 
     public Item findById(int id) {
