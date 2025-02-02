b6fdd8d9b0d4b22e919eb4ff44f90b5117b6b0e6
MaxxKs <maks-ksenzow@mail.ru>
2025-02-03 00:47:56 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #531008]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,10 +60,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            int length = size - index - 1;
-            if (length > 0) {
-                System.arraycopy(items, index + 1, items, index, length);
-            }
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
