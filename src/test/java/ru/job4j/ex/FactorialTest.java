0e326b862726def87867f3929bfc7d156dc29ee5
MaxxKs <maks-ksenzow@mail.ru>
2025-03-21 16:58:15 +0300

    0.3. Тестирование исключений с junit [#219366 #536849]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -16,4 +16,12 @@ class FactorialTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
     }
+
+    @Test
+    public void whenNumber4InResult24() {
+        int number = 4;
+        int result = new Factorial().calc(number);
+        int expected = 24;
+        assertThat(result).isEqualTo(expected);
+    }
 }
