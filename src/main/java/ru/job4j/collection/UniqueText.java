bd1097a6902a36fa52779c4df20fb5d374ea7e4f
MaxxKs <maks-ksenzow@mail.ru>
2025-05-30 19:30:22 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] duplicate = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String words : origin) {
+            check.add(words.toLowerCase());
+        }
+        for (String duplicats : duplicate) {
+            if (!check.contains(duplicats.toLowerCase())) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
